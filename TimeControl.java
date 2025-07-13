5fe7fb334b931921aec29d8ba6a6cdcafe19e18f
David Xiao <davidx1322@gmail.com>
2025-07-12 21:00:27 -0700

    Wrote time control class

--- TimeControl.java
+++ TimeControl.java
@@ -1,24 +1,51 @@
+// Class is written where time tracks miliseconds
+
 public class TimeControl {
     private long time;
     private long delay;
     private long increment;
-    public TimeControl(long t, long d, long i) {
-        time = t;
-        delay = d;
-        increment = i;
+
+    /**
+     * @param time Initial time (in seconds)
+     * @param delay Delay (in seconds)
+     * @param increment Increment (in seconds)
+     */
+    public TimeControl(long time, long delay, long increment) {
+        this.time = time * 1000;
+        this.delay = delay * 1000;
+        this.increment = increment * 1000;
     }
 
+    /**
+     * Create a time control with unlimited time
+     */
     public TimeControl() {
         time = Long.MAX_VALUE;
-        delay = Long.MAX_VALUE;
+        delay = 0;
         increment = 0;
     }
 
-    public void start() {
-        return;
+    /**
+     * @param time_spent Time spent my player (in miliseconds)
+     * @return If player has ran out of time, return false. Otherwise return true. 
+     */
+    public boolean updateClock(long time_spent) {
+        time -= Math.max(0, time_spent - delay);
+        
+        if (time <= 0) {
+            time = 0;
+            return false;
+        }
+        
+        time += increment;
+
+        return true;
     }
 
-    public void end() {
-        return;
+    /**
+     * @return Returns time left on player clock in miliseconds
+     */
+    public long getTime() {
+        return time;
     }
 }
